fc67f7673718c2a6bdd67c58750ad1d59437769a
Valentin Buttini <buttiniprocer@hotmail.com>
2022-08-01 17:01:53 -0300

    primer commit

---
+++ archivo.js
@@ -0,0 +1,43 @@
+function solicitarDestino() {
+  let destino = prompt("Ingresar su destino elegido");
+  alert("su destino elegido   es " + destino);
+}
+let cantidadPersonas=0;
+function solicitarCantidad() {
+  cantidadPersonas = parseInt(prompt("Ingresar cantidad de personas a realizar la excursion en numeros"));
+  alert("La cantidad de personas  es " + cantidadPersonas);
+  
+}
+
+function edad() {
+  let edad = prompt("ingrese si son (Mayor de edad o Menor de edad)");
+  switch(edad) {
+    case "Mayor de edad":
+    case "mayor de edad":
+    case "MAYOR DE EDAD":  
+      alert("si puede realizar el treking");
+      break;
+    case "Menor de edad":
+    case "menor de edad":
+    case "MENOR DE EDAD":  
+      alert("no puede realizar el treking")
+      break;
+    default:
+      alert("ingrese la edad")
+      let edad = prompt("ingrese si son (Mayor de edad o Menor de edad)");
+      break;
+   }   
+}
+let ESPACIO = " ";
+num1=1000;
+let multiplicacion=0;
+function calcularPrecio() {
+  multiplicacion= cantidadPersonas*num1;
+  alert("el precio final para" + ESPACIO + cantidadPersonas + ESPACIO + "personas" + ESPACIO + "es " + multiplicacion)
+}
+
+
+solicitarDestino();
+solicitarCantidad();
+edad();
+calcularPrecio();
