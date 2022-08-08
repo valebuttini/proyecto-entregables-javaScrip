6d8ea23e15d0d7e8281063e7b8595c5928e2b6f0
Valentin Buttini <buttiniprocer@hotmail.com>
2022-08-08 12:03:33 -0300

    realice map,find,filter

--- archivo.js
+++ archivo.js
@@ -5,15 +5,15 @@ function solicitarDestino() {
   alert("su destino elegido   es " + destino);
   if (destino == "Valle grande".toLowerCase()) {
     alert(
-      "sus desafios disponibles son: \nid: 12345-nombre:Avistaje Los Condores \nid: 54321-nombre:Las Agatas \nid: 123321-nombre:Montaña el Blue"
+      "sus desafios disponibles son: \nid: 12345 - nombre: Avistaje Los Condores - Dificultad - 4 \nid: 54321 - nombre: Las Agatas - Dificultad - 3 \nid: 123321 - nombre: Montaña el Blue - Dificultad - 4"
     );
   } else if (destino == "El tigre".toLowerCase()) {
     alert(
-      "sus desafios disponibles son: \nid: 98765-nombre:Cerro el Tigre \nid: 67543-nombre:Cerro Gallor  \nid: 5432112-nombre:Cerro 360"
+      "sus desafios disponibles son: \nid: 98765 - nombre: Cerro el Tigre - Dificultad - 1 \nid: 67543 - nombre: Cerro Gallor - Dificultad - 3  \nid: 5432112 - nombre: Cerro 360 - Dificultad - 1"
     );
   } else if (destino == "los reyunos".toLowerCase()) {
     alert(
-      "sus desafios disponibles son: \nid: 23415-nombre:Cortaderas \nid: 23415-nombre:Cerro Los Reyunos  \nid: 98763-nombre:Los gatos"
+      "sus desafios disponibles son: \nid: 23415 - nombre: Cortaderas - Dificultad - 2 \nid: 23415 - nombre: Cerro Los Reyunos - Dificultad - 2  \nid: 98763 - nombre: Los gatos - Dificultad - 3"
     );
   } else {
     alert("ingresa tu destino");
@@ -35,7 +35,7 @@ const desafios = [
   {
     id: "12345",
     nombre: "Avistaje Los Condores",
-    dificultad: "Nivel 1",
+    dificultad: 4,
     precio: 1500,
     horas: 2,
     disponibilidad: true,
@@ -43,7 +43,7 @@ const desafios = [
   {
     id: "54321",
     nombre: "Las agatas",
-    dificultad: "Nivel 3",
+    dificultad: 3,
     precio: 2000,
     horas: 3,
     disponibilidad: false,
@@ -51,7 +51,7 @@ const desafios = [
   {
     id: "12332",
     nombre: "Montañana el Blue",
-    dificultad: "Nivel 4",
+    dificultad: 4,
     precio: 2800,
     horas: 5,
     disponibilidad: true,
@@ -59,7 +59,7 @@ const desafios = [
   {
     id: "54321",
     nombre: "Cerro 360",
-    dificultad: "Nivel 5",
+    dificultad: 1,
     precio: 3000,
     horas: 6,
     disponibilidad: true,
@@ -67,7 +67,7 @@ const desafios = [
   {
     id: "98765",
     nombre: "Cerro el Tigre",
-    dificultad: "Nivel 6",
+    dificultad: 3,
     precio: 3000,
     horas: 7,
     disponibilidad: true,
@@ -75,7 +75,7 @@ const desafios = [
   {
     id: "67543",
     nombre: "Cerro Gallo",
-    dificultad: "Nivel 1",
+    dificultad: 1,
     precio: 2300,
     horas: 2,
     disponibilidad: true,
@@ -83,7 +83,7 @@ const desafios = [
   {
     id: "98763",
     nombre: "Los gatos",
-    dificultad: "Nivel 2",
+    dificultad: 2,
     precio: 1400,
     horas: 4,
     disponibilidad: true,
@@ -91,7 +91,7 @@ const desafios = [
   {
     id: "23416",
     nombre: "Cerro Los Reyunos",
-    dificultad: "Nivel 5",
+    dificultad: 2 ,
     precio: 3000,
     horas: 6,
     disponibilidad: false,
@@ -99,33 +99,64 @@ const desafios = [
   {
     id: "23415",
     nombre: "Cortaderas",
-    dificultad: "Nivel 5",
+    dificultad: 3,
     precio: 1700,
     horas: 3,
     disponibilidad: true,
   },
 ];
+let dificultadUsuario = parseInt(prompt("ingrese la dificultad apta para las personas a desarrollar la excursion"));
+const dificultades = desafios.filter((el)=> el.dificultad == dificultadUsuario );
 
-let id = prompt("ingresa el Id de tu desafio elegido");
-let trekingDesafio;
-let resultado = 0;
-for (const treking of desafios) {
-  if (treking.id == id) {
-    alert("tu desafio elegido es:");
-    let aString =
-      "\nID:" +
-      treking.id +
-      "\nNombre:" +
-      treking.nombre +
-      "\nDificultad:" +
-      treking.dificultad +
-      "\nDisponibilidad:" +
-      treking.disponibilidad +
-      "\nPrecio por persona $:" +
-      treking.precio +
-      "\nHoras:" +
-      treking.horas;
-    alert(aString);
-    let trekingDesafio = treking;
-  }
+console.table(dificultades);
+
+{
+  const respuestaUsuario = (idDesafio) =>  {
+  
+    const {
+  
+      id, 
+  
+      nombre, 
+  
+      dificultad, 
+  
+      disponibilidad, 
+  
+      precio, 
+  
+      horas
+  
+  } = desafios.find( desafio => desafio.id == idDesafio );
+  
+    let aString =`
+  
+      ID: ${id}
+  
+      Nombre: ${nombre}
+  
+      Dificultad: ${dificultad}
+  
+      Disponibilidad: ${disponibilidad}
+  
+      Precio por persona $: ${precio}
+  
+      Horas: ${horas}
+  
+    `;
+  
+      alert(aString);
+  
+  };
+  respuestaUsuario(prompt("ingresa el Id de tu desafio elegido"));
+  
 }
+
+const actualizarPrecios = desafios.map((el) =>{
+  return{
+    nombre: el.nombre,
+    precio: el.precio +(el.precio*0.21)
+  }
+})
+console.log("los precios con  IVA son:")
+console.log(actualizarPrecios)
