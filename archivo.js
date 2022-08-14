d93615f8fc88ba5e1a5acbfe70f958fec2fd661d
Valentin Buttini <buttiniprocer@hotmail.com>
2022-08-14 12:05:44 -0300

    realice secciones,imagenes,cards,style

--- archivo.js
+++ archivo.js
@@ -1,38 +1,16 @@
-// funciones solicitantes de datos mediante if y else
+class destino{
+  constructor(nombre, ruta, imagenD) {
+      this.nombre = nombre;
+      this.ruta  = ruta;
+      this.imagenD = imagenD;
+  }
+}
+const destino1 = new destino("Los Reyunos", "Ruta Provincial 173","./imagenes/reyunosDestino.jpg");
+const destino2 = new destino("Valle Grande", "Ruta Nacional 143","./imagenes/valleDestino.jpg");
+const destino3 = new destino("El tigre", "Ruta Nacional 150","./imagenes/tigreDestino.jpg");
 
-// function solicitarDestino() {
-//   let destino = prompt(
-//     "Elige e Ingrese su destino  \nValle Grande \nLos Reyunos \nEl Tigre "
-//   );
-//   alert("su destino elegido   es " + destino);
-//   if (destino == "Valle grande".toLowerCase()) {
-//     alert(
-//       "sus desafios disponibles son: \nid: 12345 - nombre: Avistaje Los Condores - Dificultad - 4 \nid: 54321 - nombre: Las Agatas - Dificultad - 3 \nid: 123321 - nombre: Montaña el Blue - Dificultad - 4"
-//     );
-//   } else if (destino == "El tigre".toLowerCase()) {
-//     alert(
-//       "sus desafios disponibles son: \nid: 98765 - nombre: Cerro el Tigre - Dificultad - 1 \nid: 67543 - nombre: Cerro Gallor - Dificultad - 3  \nid: 5432112 - nombre: Cerro 360 - Dificultad - 1"
-//     );
-//   } else if (destino == "los reyunos".toLowerCase()) {
-//     alert(
-//       "sus desafios disponibles son: \nid: 23415 - nombre: Cortaderas - Dificultad - 2 \nid: 23415 - nombre: Cerro Los Reyunos - Dificultad - 2  \nid: 98763 - nombre: Los gatos - Dificultad - 3"
-//     );
-//   } else {
-//     alert("ingresa tu destino");
-//   }
-// }
-// let cantidadPersonas = 0;
-// function solicitarCantidad() {
-//   cantidadPersonas = parseInt(
-//     prompt(
-//       "Bienvenidos!! Ingresar cantidad de personas a realizar la excursion en numeros"
-//     )
-//   );
-//   alert("La cantidad de personas  es " + cantidadPersonas);
-// }
 
-// solicitarCantidad();
-// solicitarDestino();
+
 
 const desafio1 ={
     id: "12345",
@@ -90,96 +68,41 @@ const desafio6={
     imagen:"./imagenes/cerroGallo.JPG"
  
 }
-  
 
 
-
-
-
-
-
-
-  // {
-  //    id: "98763",
-  //    nombre: "Los gatos",
-  //    dificultad: 2,
-  //    precio: 1400,
-  //    horas: 4,
-  //   disponibilidad: true
-  // }
-
-  // {
-  //    id: "23416",
-  //    nombre: "Cerro Los Reyunos",
-  //    dificultad: 2,
-  //    precio: 3000,
-  //    horas: 6,
-  //    disponibilidad: false
-
-  // }
-
-  // {
-  //    id: "23415",
-  //    nombre: "Cortaderas",
-  //    dificultad: 3,
-  //    precio: 1700,
-  //    horas: 3,
-  //    disponibilidad: true
-  // }
-
-
-// let dificultadUsuario = parseInt(
-//   prompt(
-//     "ingrese la dificultad apta para las personas a desarrollar la excursion"
-//   )
-// );
-// metodo de filtrado
-
-// const dificultades = desafios.filter(
-//   (el) => el.dificultad == dificultadUsuario
-// );
-
-// console.table(dificultades);
-// busqueda por id
-// {
-//   const idUsuario = (idDesafio) => {
-//     const {
-//       id,
-
-//       nombre,
-
-//       dificultad,
-
-//       disponibilidad,
-
-//       precio,
-
-//       horas,
-//     } = desafios.find((desafio) => desafio.id == idDesafio);
-
-//     let aString = `
-//       ID: ${id}
-//       Nombre: ${nombre}
-//       Dificultad: ${dificultad}
-//       Disponibilidad: ${disponibilidad}
-//       Precio por persona $: ${precio}
-//       Horas: ${horas}`;
-
-//     alert(aString);
-//   };
-//   idUsuario(prompt("ingresa el Id de tu desafio elegido"));
-// }
-
-// nuevo array con precion actualizados mas iva
-// const actualizarPrecios = desafios.map((el) => {
-//   return {
-//     nombre: el.nombre,
-//     precio: (el.precio +( el.precio * 0.21)),
-//   };
-// });
-// console.log("los precios con  IVA son:");
-// console.log(actualizarPrecios);
-
+let tarjetaIndex1=document.getElementById("tarjetaIndex1");
+tarjetaIndex1.innerHTML=`
+        <div class="card" style="width: 18rem;">
+              <img src="${destino1.imagenD}" class="card-img-top" alt="avistaje de condores">
+              <div class="card-body">
+                <h5 class="card-title">${destino1.nombre}</h5>
+                <p class="card-text">${destino1.ruta}</p>
+                <a href="../paginas/desafios.html" class="btn btn-primary">Comprar</a>
+              </div>
+        </div>
+`;
+let tarjetaIndex2=document.getElementById("tarjetaIndex2");
+tarjetaIndex2.innerHTML=`
+        <div class="card" style="width: 18rem;">
+              <img src="${destino2.imagenD}" class="card-img-top" alt="avistaje de condores">
+              <div class="card-body">
+                <h5 class="card-title">${destino2.nombre}</h5>
+                <p class="card-text">${destino2.ruta}</p>
+                <a href="#" class="btn btn-primary">Comprar</a>
+              </div>
+        </div>
+`;
+let tarjetaIndex3=document.getElementById("tarjetaIndex3");
+tarjetaIndex3.innerHTML=`
+        <div class="card" style="width: 18rem;">
+              <img src="${destino3.imagenD}" class="card-img-top" alt="avistaje de condores">
+              <div class="card-body">
+                <h5 class="card-title">${destino3.nombre}</h5>
+                <p class="card-text">${destino3.ruta}</p>
+                <a href="#" class="btn btn-primary">Comprar</a>
+              </div>
+        </div>
+`;
 
 let tarjeta1=document.getElementById("tarjeta1");
 tarjeta1.innerHTML=`
@@ -247,3 +170,7 @@ tarjeta6.innerHTML=`
               </div>
         </div>
 `;
+
+
+let listaF = document.getElementById("listaF")
+listaF.innerHTML = "<ul><li>INSTAGRAM</li><li>FACEBOOK</li><li>WATHSAAP</li></ul>"
